1464a5d3336fce0eb417a8ea816710160c494906
Vedran Milković <vedran.milkovic25@gmail.com>
2022-05-24 21:38:59 +0200

    Recursive Common Table Expressions

---
+++ Section 27/recursive_cte.sql
@@ -0,0 +1,23 @@
+WITH RECURSIVE countdown(val) AS (
+    SELECT 10 AS val
+    UNION
+    SELECT val - 1 FROM countdown WHERE val > 1
+)
+SELECT *
+FROM countdown;
+
+WITH RECURSIVE suggestions(leader_id, follower_id, depth) AS (
+        SELECT leader_id, follower_id, 1 AS depth
+        FROM followers
+        WHERE follower_id = 1000
+    UNION
+        SELECT followers.leader_id, followers.follower_id, depth + 1
+        FROM followers
+        JOIN suggestions ON suggestions.leader_id = followers.follower_id
+        WHERE depth < 3
+)
+SELECT DISTINCT users.id, users.username
+FROM suggestions
+JOIN users ON users.id = suggestions.leader_id
+WHERE depth > 1
+LIMIT 5;
